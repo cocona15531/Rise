3cb3c58a8f8a6ec613b4de10fa5f7a5c6344e7df
issei.ueda <ron.ron633662@gmail.com>
2023-11-21 19:48:05 +0900

    タイマー機能を追加

--- Rise/View/MeditationViewController.swift
+++ Rise/View/MeditationViewController.swift
@@ -6,17 +6,25 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class MeditationViewController: UIViewController {
 
     @IBOutlet weak var meditationLabel: UILabel!
-    
     @IBOutlet weak var countLabel: UILabel!
+    
+    var timer: Timer?
+    var initialCountdownSeconds = 5  // 初期カウントダウン（5秒）
+    var mainCountdownSeconds = 61    // 本カウントダウン（60秒）
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = "6"
+        startInitialCountdown()
 
-        // Do any additional setup after loading the view.
     }
+    
+    
 
     @IBAction func goBackHomeButton(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
@@ -44,6 +52,38 @@ class MeditationViewController: UIViewController {
                     whiteView.removeFromSuperview()
                 }
             }
+        }
+    }
+    
+    func startInitialCountdown() {
+        // 初期カウントダウン用のタイマーを設定
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateInitialCountdown), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateInitialCountdown() {
+        if initialCountdownSeconds > 0 {
+            countLabel.text = "\(initialCountdownSeconds)"
+            initialCountdownSeconds -= 1
+        } else {
+            timer?.invalidate()
+            startMainCountdown()  // 本カウントダウンを開始
+        }
+    }
+
+    func startMainCountdown() {
+        // 本カウントダウン用のタイマーを設定
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateMainCountdown), userInfo: nil, repeats: true)
+    }
+
+    @objc func updateMainCountdown() {
+        if mainCountdownSeconds > 0 {
+            mainCountdownSeconds -= 1
+            countLabel.text = "\(mainCountdownSeconds)"
+        } else {
+            timer?.invalidate()
+            // カウントダウンが終了したときの処理をここに書く
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+
         }
     }
 }
